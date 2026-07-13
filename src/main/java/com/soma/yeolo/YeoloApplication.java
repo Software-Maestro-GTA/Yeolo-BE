06673eadc3168a10f9ceb4ddb96d43212c8ad7fe
한승우 <hsw@hanseung-uui-MacBookAir.local>
2026-07-13 12:22:23 +0900

    Spring Boot 초기화

---
+++ src/main/java/com/soma/yeolo/YeoloApplication.java
@@ -0,0 +1,13 @@
+package com.soma.yeolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeoloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeoloApplication.class, args);
+    }
+
+}
